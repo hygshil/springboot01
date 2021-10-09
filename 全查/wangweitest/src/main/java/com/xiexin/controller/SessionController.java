cf0387908e7ddc7e7306e464a33970d921787734
hyg <hygshl1@163.com>
2021-10-09 08:23:48 +0800

    quancha

---
+++ 全查/wangweitest/src/main/java/com/xiexin/controller/SessionController.java
@@ -0,0 +1,23 @@
+package com.xiexin.controller;
+
+import com.xiexin.bean.SessionLog;
+import com.xiexin.respcode.Result;
+import com.xiexin.service.SessionLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/sessionlog")
+public class SessionController {  ///sessionlog/selectAll
+    @Autowired
+    private SessionLogService sessionLogService;
+    @RequestMapping("/selectAll")
+    public Result selectAll(){
+        List<SessionLog> sessionLogs = sessionLogService.selectByExample(null);
+        Result result = new Result(sessionLogs);
+        return result;
+    }
+}
---
+++ 全查/wangweitest/src/main/java/com/xiexin/controller/SysController.java
@@ -0,0 +1,22 @@
+package com.xiexin.controller;
+
+import com.xiexin.bean.SysLog;
+import com.xiexin.respcode.Result;
+import com.xiexin.service.SysLogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/syslog")
+public class SysController {
+    @Autowired
+    private SysLogService sysLogService;
+    @RequestMapping("/selectAll")
+    public Result selectAll(){
+        List<SysLog> sysLogs = sysLogService.selectByExample(null);
+        Result result = new Result(sysLogs);
+        return result;
+    }
+}
